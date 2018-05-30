4999c36c53ea968942c406d4a54aefaeb2f8c6e9
Toan Luong <toan.luongm@gmail.com>
2018-05-29 19:00:58 -0700

    Add compilable real_demo.py and sim_demo.py

--- examples/real_demo.py
+++ examples/real_demo.py
@@ -1,50 +1,87 @@
-def filter_pair(label_pair, x, y):
-    """ Filter a multi-class dataset into binary dataset given a label pair.
-    """
-    mask = np.isin(y, label_pair)
-    x_bin, y_bin = x[mask].copy(), y[mask].copy()
-    y_bin[y_bin==label_pair[0]] = 1.0
-    y_bin[y_bin==label_pair[1]] = -1.0
-    return x_bin, y_bin
+from tqdm import tqdm
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import mode
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_digits
 
-def evaluate(beta, X_train, X_test, y_test, kernel, **kwargs):
-    n_test = len(y_test)
-    y_pred = np.zeros(n_test)
-    y_vals = np.zeros(n_test)
-    for i in range(n_test):
-        y_vals[i] = np.dot(kernel(X_train, X_test[i, :].reshape(1, -1), **kwargs).reshape(-1), beta)
-    y_pred = np.sign(y_vals)
-    return np.mean(y_pred != y_test), y_vals  # return error and values from before applying cutoff
+import sys
+sys.path.insert(0, '../')
+from src.svm import HuberSVM
+    
+def real_demo(verbose=True, plot_progress=True):
+    def filter_pair(label_pair, x, y):
+        """ Filter a multi-class dataset into binary dataset given a label pair.
+        """
+        mask = np.isin(y, label_pair)
+        x_bin, y_bin = x[mask].copy(), y[mask].copy()
+        y_bin[y_bin==label_pair[0]] = 1.0
+        y_bin[y_bin==label_pair[1]] = -1.0
+        return x_bin, y_bin
 
-def train_predict(X_train, y_train, X_test, y_test, lam, method='ovo', **config):
-    error = None
-    label_list = np.unique(y_train)
-    if method == 'ovo':
-        pred_list = []
-        label_pair_list = list(itertools.combinations(label_list, 2))
-        for label_pair in tqdm(label_pair_list):
-            X_train_bin, y_train_bin = filter_pair(label_pair, X_train, y_train)
-            beta_vals, train_cache = mylinearsvm(X_train_bin, y_train_bin, lam, **config)
-            if config['plot']:
-                plt.show(train_cache['plot'])
-            _, scores = predict(beta_vals[-1, :], X_train_bin, X_test, gram, **train_cache)
-            y_pred_bin = np.zeros_like(y_test) + label_pair[-1]
-            y_pred_bin[scores >= 0] = label_pair[0]
-            pred_list.append(y_pred_bin)
-        test_preds = np.array([mode(pi).mode[0] for pi in np.array(pred_list, dtype=np.int64).T])
-        error = np.mean(test_preds != y_test)
-    elif method == 'ovr':
-        score_list = []
-        for label in tqdm(label_list):
-            y_train_bin = np.zeros_like(y_train) - 1
-            y_train_bin[y_train == label] = 1
-            beta_vals, train_cache = mylinearsvm(X_train, y_train_bin, lam, **config)
-            if config['plot']:
-                plt.show(train_cache['plot'])
-            scores = predict(beta_vals[-1, :], X_train, X_test, gram, **train_cache)
-            score_list.append(scores)
-        test_preds = np.argmax(np.stack(score_list, axis=1), axis=1)
-        error = np.mean(test_preds != y_test)
-    else:
-        print("Method Not Implemented")
-    return error, beta_vals
+    def evaluate(beta, X_train, X_test, kernel, **kwargs):
+        n_test = X_test.shape[0]
+        y_pred = np.zeros(n_test)
+        y_vals = np.zeros(n_test)
+        for i in range(n_test):
+            y_vals[i] = np.dot(kernel(X_train, X_test[i, :].reshape(1, -1), **kwargs).reshape(-1), beta)
+        return y_vals
+
+    def train_predict(X_train, y_train, X_test, y_test, lam, method='ovo', **config):
+        error = None
+        label_list = np.unique(y_train)
+        if method == 'ovo':
+            pred_list = []
+            label_pair_list = list(itertools.combinations(label_list, 2))
+            for label_pair in tqdm(label_pair_list):
+                X_train_bin, y_train_bin = filter_pair(label_pair, X_train, y_train)
+                mylinearsvm = HuberSVM()
+                mylinearsvm.fit(X_train_bin, y_train_bin, lam, **config)
+                beta_vals, train_cache = mylinearsvm.beta_vals, mylinearsvm.cache
+                if config['plot']:
+                    plt.show(train_cache['plot'])
+                scores = evaluate(beta_vals[-1, :], X_train_bin, X_test, mylinearsvm.gram, **train_cache)
+                y_pred_bin = np.zeros_like(y_test) + label_pair[-1]
+                y_pred_bin[scores >= 0] = label_pair[0]
+                pred_list.append(y_pred_bin)
+            test_preds = np.array([mode(pi).mode[0] for pi in np.array(pred_list, dtype=np.int64).T])
+            error = np.mean(test_preds != y_test)
+        elif method == 'ovr':
+            score_list = []
+            for label in tqdm(label_list):
+                y_train_bin = np.zeros_like(y_train) - 1
+                y_train_bin[y_train == label] = 1
+                mylinearsvm = HuberSVM()
+                mylinearsvm.fit(X_train, y_train_bin, lam, **config)
+                beta_vals, train_cache = mylinearsvm.beta_vals, mylinearsvm.cache
+                if config['plot']:
+                    plt.show(train_cache['plot'])
+                scores = evaluate(beta_vals[-1, :], X_train, X_test, mylinearsvm.gram, **train_cache)
+                score_list.append(scores)
+            test_preds = np.argmax(np.stack(score_list, axis=1), axis=1)
+            error = np.mean(test_preds != y_test)
+        else:
+            print("Method Not Implemented")
+        return error, beta_vals
+    
+    print("Download the dataset...")
+    digits = load_digits() 
+    num_images = digits.images.shape[0]
+    X_train, X_test, y_train, y_test = train_test_split(digits.images.reshape(num_images, -1), digits.target, test_size=.4, random_state=42)
+    #Standardizing the data
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+    if verbose:
+        print('Number of training examples:', X_train.shape)
+        print('Number of test examples:', X_test.shape)
+    lambda_list = [1]
+    for l in lambda_list:
+        error, beta_vals = train_predict(X_train, y_train, X_test, y_test, lam=l, method='ovr', **{'kernel_choice': 'linear', 'sigma': 1, 'plot': plot_progress, 'max_iter': 50})
+        print("Lambda = %0.4f. Misclassification Error = %0.4f" %(l, error))
+
+if __name__=='__main__':
+    real_demo(verbose=True, plot_progress=False)
--- examples/sim_demo.py
+++ examples/sim_demo.py
@@ -9,66 +9,77 @@ import sys
 sys.path.insert(0, '../')
 from src.svm import HuberSVM
 
-class Simulated_Demo:
-    def __init__(self, verbose=True, plot=False)
-        self.X, self.Xy = sklearn.datasets.make_circles(noise=0.2, factor=0.5, random_state=1)
-        self.X = StandardScaler().fit_transform(X)
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=.4, random_state=42)
-        self.y_train = 2*self.y_train - 1
-        self.y_test = 2*self.y_test - 1
-        if verbose:
-            print('Number of training examples:', self.X_train.shape[0])
-            print('Number of test examples:', self.X_test.shape[0])
-        if plot:
-            x_min, x_max = self.X[:, 0].min() - .5, self.X[:, 0].max() + .5
-            y_min, y_max = self.X[:, 1].min() - .5, self.X[:, 1].max() + .5
-            h = .02  # step size in the mesh
-            self.xx, self.yy = np.meshgrid(np.arange(x_min, x_max, h),
-                                 np.arange(y_min, y_max, h))
-
-            self.cm = plt.cm.RdBu
-            self.cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-            # Plot the training points
-            ax = plt.subplot()
-            ax.scatter(self.X_train[:, 0], self.X_train[:, 1], c=self.y_train, cmap=self.cm_bright,
-                       edgecolors='k')
-            # and testing points
-            ax.scatter(self.X_test[:, 0], self.X_test[:, 1], c=self.y_test, cmap=self.cm_bright, alpha=0.1,
-                       edgecolors='k')
-            ax.set_xlim(self.xx.min(), self.xx.max())
-            ax.set_ylim(self.yy.min(), self.yy.max())
-            ax.set_xticks(())
-            ax.set_yticks(())
-            plt.show()
-    def evaluate(self, beta, X_test, y_test, kernel, **kwargs):
+def sim_demo(verbose=True, plot_contour=False):
+    def evaluate(beta, X_train, X_test, y_test, kernel, **kwargs):
         n_test = len(y_test)
         y_pred = np.zeros(n_test)
         y_vals = np.zeros(n_test)
         for i in range(n_test):
-            y_vals[i] = np.dot(kernel(self.X_train, X_test[i, :].reshape(1, -1), **kwargs).reshape(-1), beta)
+            y_vals[i] = np.dot(kernel(X_train, X_test[i, :].reshape(1, -1), **kwargs).reshape(-1), beta)
         y_pred = np.sign(y_vals)
         return np.mean(y_pred != y_test), y_vals  # return error and values from before applying cutoff
     
-    def plot(self, betas, kernel, **kwargs):
-        Zs = np.c_[self.xx.ravel(), self.yy.ravel()]
-        error, test_values = misclassification_error(betas[-1, :], self.X_train, self.X_test, self.y_test, kernel, **kwargs)
-        Z = misclassification_error(betas[-1, :], X_train, Zs, [0]*len(Zs), kernel, **kwargs)[1]
+    def evaluate_plot(betas, kernel, plot_contour, **kwargs):
+        error, test_values = evaluate(betas[-1, :], X_train, X_test, y_test, kernel, **kwargs)
         print('Misclassification error when lambda =', kwargs['lambda'], ':', error)
+        ax = None
+        if plot_contour:
+            Zs = np.c_[xx.ravel(), yy.ravel()]
+            Z = evaluate(betas[-1, :], X_train, Zs, [0]*len(Zs), kernel, **kwargs)[1]
+            # Put the result into a color plot
+            Z = Z.reshape(xx.shape)
+            ax = plt.subplot()
+            ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
 
-        # Put the result into a color plot
-        Z = Z.reshape(xx.shape)
+            # Plot also the training points
+            ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
+                       edgecolors='k')
+            # and testing points
+            ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                       edgecolors='k', alpha=0.2)
+
+            ax.set_xlim(xx.min(), xx.max())
+            ax.set_ylim(yy.min(), yy.max())
+            ax.set_xticks(())
+            ax.set_yticks(())
+        return error, test_values, ax
+        
+    X, y = sklearn.datasets.make_circles(noise=0.2, factor=0.5, random_state=1)
+    X = StandardScaler().fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4, random_state=42)
+    y_train = 2*y_train - 1
+    y_test = 2*y_test - 1
+    if verbose:
+        print('Number of training examples:', X_train.shape[0])
+        print('Number of test examples:', X_test.shape[0])
+
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = .02  # step size in the mesh
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    cm = plt.cm.RdBu
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    if plot_contour:
+        # Plot the training points
         ax = plt.subplot()
-        ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
-
-        # Plot also the training points
         ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
                    edgecolors='k')
         # and testing points
-        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
-                   edgecolors='k', alpha=0.2)
-
+        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.1,
+                   edgecolors='k')
         ax.set_xlim(xx.min(), xx.max())
         ax.set_ylim(yy.min(), yy.max())
         ax.set_xticks(())
         ax.set_yticks(())
-        plt.show()
+
+    mysvm = HuberSVM()
+    mysvm.fit(X_train, y_train, lam=0.8, **{'plot': True, 'kernel_choice': 'rbf', 'sigma': 1, 'max_iter': 10})
+    beta_vals = mysvm.beta_vals
+    train_cache = mysvm.cache
+    errors, test_vals, contour_ax = evaluate_plot(beta_vals, mysvm.gram, plot_contour, **train_cache)
+    plt.show()
+    
+if __name__=='__main__':
+    sim_demo(verbose=True, plot_contour=True)
